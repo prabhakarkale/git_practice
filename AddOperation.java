e780ba9e063424523817c0033291ccae0e96238d
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-03 13:08:23 +0530

    added AddOperation.java file with method add(a,b)

---
+++ AddOperation.java
@@ -0,0 +1,12 @@
+class AddOperation
+{
+    public static void main(String args[])
+    {
+        int i = 10, j = 20;
+        System.out.println("Addition : " + add(i , j));
+    }
+    int add(int a, int b)
+    {
+        return a+b;
+    }
+}
