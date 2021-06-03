8cd86a226f0f467358c6d5c418e8558561e646e4
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-03 13:20:05 +0530

    added MultiOperation.java file with multi(a,b) method

---
+++ MultiOperation.java
@@ -0,0 +1,12 @@
+class MultiOperation
+{
+    public static void main(String args[])
+    {
+        int i = 10, j = 20;
+        System.out.println("Multification : " + multi(i , j));
+    }
+    int multi(int a, int b)
+    {
+        return (a * b);
+    }
+}
